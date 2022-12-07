1dbe7223b7e9580ae5bb783e90801a5b385907b4
MariusJohn <marius.ciuclea@gmail.com>
2022-12-07 10:53:40 +0000

    added new classess in index.js file

--- src/index.js
+++ src/index.js
@@ -10,8 +10,50 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares:squares});
+  }
+  renderSquare(i){
+    return (
+    <Square 
+    value ={this.state.square[i]} 
+    onClick={() => this.handleClick(i)}/>
+    );
+  }
+}
+
+
+
+class Square extends React.Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     value: null,
+  //   };
+  // }
+  render(){
+    return(
+      <button className="sqare" 
+      onClick={()=> this.props.onclick()}
+      >
+        {this.props.vale}
+      </button>
+    );
+  }
+}
+
